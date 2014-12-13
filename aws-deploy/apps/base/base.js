96dba92cd84eca63cba01b774cdc5b09b0ac03ce
Jesper Svennevid <github@svennevid.se>
2014-12-13 17:09:33 +0100

    Added toastr for user responses

--- aws-deploy/apps/base/base.js
+++ aws-deploy/apps/base/base.js
@@ -12,7 +12,8 @@ module.exports = Application.create({
             "//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.js",
             "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.js",
             "//cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.3.0/bootbox.js",
-            "//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone.js"
+            "//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone.js",
+            "//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.js"
         ],
         "common": [
             "js/scripts/*.js",
@@ -29,7 +30,8 @@ module.exports = Application.create({
         "lib": [
             "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/css/bootstrap.css",
             "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/css/bootstrap-theme.css",
-            "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.css"
+            "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.css",
+            "//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.css"
         ],
         "common": [
             "css/*.css"
