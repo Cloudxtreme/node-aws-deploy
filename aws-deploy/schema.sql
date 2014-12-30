ad10083a59cb421852f5be9a64d70f67f8cc27ac
Jesper Svennevid <github@svennevid.se>
2014-12-30 22:58:43 +0100

    Updated db schema

--- aws-deploy/schema.sql
+++ aws-deploy/schema.sql
@@ -112,6 +112,7 @@ CREATE TABLE `awd_repositories` (
   `repository_state` varchar(64) NOT NULL,
   `repository_url` text,
   `repository_credentials` text,
+  `repository_secret` text,
   PRIMARY KEY (`deployment_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -162,4 +163,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-12-30  6:08:17
+-- Dump completed on 2014-12-30 22:56:33
